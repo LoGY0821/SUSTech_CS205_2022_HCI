d754041f7c4d50ea7c1cacc7f24420697f64ba00
Smangic <1255567856@qq.com>
2022-06-19 11:15:45 +0800

    submit version

--- CMakeFiles/C_Test.dir/CNNModelTraining.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/C_Test.dir/CNNModelTraining.cpp.obj: \
- E:\Windows\OneDrive\Documents\Self\ Document\Programming\ Base\CPP\Project_HCI\SUSTech_CS205_2022_HCI\CNNModelTraining.cpp
--- CMakeFiles/C_Test.dir/hull.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/C_Test.dir/hull.cpp.obj: \
- E:\Windows\OneDrive\Documents\Self\ Document\Programming\ Base\CPP\Project_HCI\SUSTech_CS205_2022_HCI\hull.cpp
