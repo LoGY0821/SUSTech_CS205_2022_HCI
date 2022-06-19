2ff2373523b7655648ad19e5aa478470383a6cca
Smangic <1255567856@qq.com>
2022-06-19 11:11:48 +0800

    The Final Version

--- HCI.cpp
+++ HCI.cpp
@@ -6,6 +6,9 @@
 #include <opencv2/ml.hpp>
 #include <fstream>
 #include <opencv2/imgproc/imgproc_c.h>
+#include <windows.h>
+#include <time.h>
+
 using namespace cv;
 
 Mat frame,binary,frame_mirror; //the current frame, binary image of skin,mirror image of current frame
@@ -18,14 +21,21 @@ Point2f current_point(0,0); //to store the center of the hand
 std::vector<Point2f> track; //to store the track of the hand
 
 int mode;//0 1 2 3 4
+clock_t t1, t2;//定时器变量
+
 
 
 void getHand();
 void getSkin();
+void getskin();
 int getConvexHull();
 void getCurrentCenter();
 void drawTrack();
 void clearTrack();
+void shapeRecog(std::vector<Point2f> &recog_trace, std::string &shape);
+void contourGene(std::vector<Point2f> &points, std::vector<std::vector<Point>> &trace);
+void drawCircle(Mat &Image, std::vector<Point2f> &points);
+void virtualMouse();
 
 int main()
 {
@@ -33,6 +43,15 @@ int main()
     cap.set(3,1080);
     cap.set(4,720);
 
+    t1 = clock();
+//    //output the information of the camera
+//    std::cout << "The camera's information:" << std::endl;
+//    std::cout << "width: " << cap.get(3) << std::endl;
+//    std::cout << "height: " << cap.get(4) << std::endl;
+//    std::cout << "fps: " << cap.get(5) << std::endl;
+//    std::cout << "brightness: " << cap.get(6) << std::endl;
+//    std::cout << "contrast: " << cap.get(7) << std::endl;
+//    std::cout << "saturation: " << cap.get(8) << std::endl;
     while (cap.isOpened())
     {
         cap >> frame; //读取画面
@@ -40,13 +59,8 @@ int main()
         getSkin(); //将图片二值化，并初步提取出手
         getHand(); //找到二值图像的最大边界，应该就是手了。
 
-
         hand_contours.push_back(hand);
-        //drawContours(frame,hand_contours,0,Scalar(0,0,255),2,8); //drawContours 不能过assertion，原因未知，不能正常显示
 
-
-        getCurrentCenter();
-        drawTrack();
         clearTrack();
 
 
@@ -54,12 +68,44 @@ int main()
         Rect rect = boundingRect(hand); //boundingRect 返回手的最小矩形区域
         rectangle(frame,rect,Scalar(0,255,0),2,8); //draw the boundingRect
 
+        //virtualMouse();
 
         //Mirror the frame symmetrically
         mode = getConvexHull(); //get the convexhull of the hand
-        flip(frame,frame_mirror,1);
 
-        putText(frame_mirror, std::to_string(mode), Point(10, 60), FONT_HERSHEY_SIMPLEX, 2, Scalar(0, 0, 255), 2);
+        std::vector<Point2f> trace_ori;
+        std::vector<std::vector<Point>> output;
+        std::string shape = "null";
+        if(mode == 3) {
+            getCurrentCenter();
+            drawTrack();
+
+        }
+        if(mode == 0 && track.size() > 30) {
+            shapeRecog(trace_ori, shape);
+            contourGene(trace_ori, output);
+            if (trace_ori.size() > 4)
+            {
+                drawCircle(frame, trace_ori);
+            }
+            else
+            {
+                drawContours(frame, output, -1, Scalar(0, 255, 255), 4, 8);
+            }
+
+        }
+
+
+        flip(frame,frame_mirror,1);
+        if(mode == 4)
+            putText(frame_mirror, std::to_string(mode+1), Point(10, 60), FONT_HERSHEY_SIMPLEX, 2, Scalar(0, 0, 255), 2);
+        else if(mode == 3)
+            putText(frame_mirror, std::to_string(mode+1), Point(10, 60), FONT_HERSHEY_SIMPLEX, 2, Scalar(0, 0, 255), 2);
+        else
+            putText(frame_mirror, std::to_string(mode), Point(10, 60), FONT_HERSHEY_SIMPLEX, 2, Scalar(0, 0, 255), 2);
+        //if shape is not null, write the shape on the frame_mirror
+        if(shape != "null")
+            putText(frame_mirror, shape, Point(500, 90), FONT_HERSHEY_SIMPLEX, 2, Scalar(0, 0, 255), 2);
 
         //predict the result
         //predictSVM(hand,frame_mirror);
@@ -75,7 +121,7 @@ int main()
 
 
 
-//get the area whose color is similar to hand color
+//use cr_cb color space to get the skin
 void getSkin()
 {
     Mat Image = frame.clone();
@@ -91,8 +137,21 @@ void getSkin()
     binary = Mat::zeros(Image.size(), CV_8UC1);
     GaussianBlur(CR, CR1, Size(3, 3), 0, 0);//对CR分量进行高斯滤波，得到CR1（注意这里一定要新建一张图片存放结果）
     threshold(CR1, binary, 0, 255, THRESH_OTSU);//用系统自带的threshold函数，对CR分量进行二值化，算法为自适应阈值的OTSU算法
+    //imshow("cr_cb", binary);
 
 }
+
+
+//use hsv color space to get the skin
+void getskin()
+{
+    Mat Image = frame.clone();
+    Mat hsvImage;
+    cvtColor(Image, hsvImage, COLOR_BGR2HSV); //转换色彩空间
+    inRange(hsvImage, Scalar(0, 43, 55), Scalar(25, 255, 255), binary);
+    //imshow("RGB", binary);
+}
+
 
 
 //get the contour of hand through a set of contours, according to the area of contours
@@ -201,13 +260,17 @@ void predictSVM(std::vector<Point>& hand, Mat& frame)
 int getConvexHull()
 {
     Mat Binary_erode;
+    Mat Binary_close;
     //the kernel is 5x5, all the element is 1
-    Mat kernel = Mat::ones(10,10,CV_8UC1);
+    Mat kernel = Mat::ones(5,5,CV_8UC1);
+    morphologyEx(binary, Binary_close, MORPH_CLOSE, kernel);
 
-    erode(binary, Binary_erode, kernel);
-    imshow("Binary_blur", Binary_erode);
+    erode(Binary_close, Binary_erode, kernel);
+    //do a closa operation on Binary_erode
+
+    //imshow("Binary_blur", Binary_close);
     std::vector<Point> Hand;
-    getHand(Binary_erode,Hand);
+    getHand(Binary_close,Hand);
     //find the convex in hand
     std::vector<Point> hull;
     std::vector<int> hull_index(Hand.size());
@@ -291,7 +354,95 @@ void drawTrack()
     //draw the track
     for(int i = 0; i < track.size(); i++)
     {
-        circle(frame,track[i],2,Scalar(100,100,100),-1);
+        circle(frame,track[i],2,Scalar(0,255,255),-1);
     }
     circle(frame,current_point,5,Scalar(255,0,0),-1); //center of the hand
+}
+
+//输出近似后的轨迹以及现状
+void shapeRecog(std::vector<Point2f> &recog_trace, std::string &shape)
+{
+    approxPolyDP(track, recog_trace, 50, true);
+    int count = (int)recog_trace.size();
+    switch (count)
+    {
+        case 2:
+            shape = "line";
+            break;
+        case 3:
+            shape = "Triangle";
+            break;
+        case 4:
+            shape = "Rectangle";
+            break;
+        default:
+            shape = "Circle";
+            break;
+    }
+}
+
+//方便画
+void contourGene(std::vector<Point2f> &points, std::vector<std::vector<Point>> &trace)
+{
+    std::vector<Point> line;
+    for (int i = 0; i < points.size(); i++)
+    {
+        line.push_back(Point((int)points[i].x, (int)points[i].y));
+    }
+    trace.push_back(line);
+}
+
+//画圆
+void drawCircle(Mat &Image, std::vector<Point2f> &points)
+{
+    int centerX=0;
+    int centerY=0;
+    int radius=0;
+    for (int i = 0; i < points.size(); i++)
+    {
+        centerX += points[i].x;
+        centerY += points[i].y;
+    }
+    centerX /= points.size();
+    centerY /= points.size();
+    for (int i = 0; i < points.size(); i++)
+    {
+        radius += sqrt(pow((points[i].x - centerX), 2) + pow((points[i].y - centerY), 2));
+    }
+    radius /= points.size();
+    circle(frame, Point(centerX, centerY), radius, Scalar(0, 255, 255), 4);
+}
+
+void virtualMouse()
+{
+    rectangle(frame, Point(160, 180), Point(800, 540), Scalar(255, 0, 0), 2);
+    Moments handMoment = moments(hand, false);
+    Point center(handMoment.m10 / handMoment.m00, handMoment.m01 / handMoment.m00);
+    int center_x = (int)center.x - 2 * ((int)center.x - 480);
+    double x = (center_x-160) * 65536 / 640;//质心转换到鼠标
+    double y = (center.y-180) * 65536 / 360;
+    if (x <= 0) x = 0;
+    else if (x >= 65536) x = 65536;
+    if (y <= 0) y = 0;
+    else if (y >= 65536) y = 65536;
+
+    t2 = clock();
+    //cout << t1 << "   " << t2 << endl;
+    if (mode == 1 && center.x > 0 && center.y > 0 && center.x > 0 && center.y > 0 && (double)(t2 - t1)/CLOCKS_PER_SEC > 1)
+    {
+        mouse_event(MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE, x, y + 3640, 0, 0);
+        mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+        t1 = t2;
+    }
+    if (mode == 2 && center.x > 0 && center.y > 0 && center.x > 0 && center.y > 0 && (double)(t2 - t1) / CLOCKS_PER_SEC > 1)
+    {
+        mouse_event(MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE, x, y + 3640, 0, 0);
+        mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+        mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+        t1 = t2;
+    }
+    if (mode == 0 && center.x >= -100000)
+    {
+        mouse_event(MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE, x, y, 0, 0);
+    }
 }
